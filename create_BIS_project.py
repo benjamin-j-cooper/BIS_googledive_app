ba1d873da5f37f64afdee8bf8a6e46215bf6e740
Ben Cooper <coop0413@umn.edu>
2023-11-29 14:28:49 -0600

    deleted .DS Store and testing .py version of app

--- create_BIS_project.py
+++ create_BIS_project.py
@@ -164,3 +164,29 @@ def main():
 
 if __name__ == "__main__":
   main()    
+
+### Possible google drive colors:
+#   "Chocolate ice cream":"#ac725e",
+#   "Old brick red":"#d06b64",
+#   "Cardinal":"#f83a22",
+#   "Wild straberries":"#fa573c",
+#   "Mars orange":"#ff7537",
+#   "Yellow cab":"#ffad46",
+#   "Spearmint":"#42d692",
+#   "Vern fern":"#16a765",
+#   "Asparagus":"#7bd148",
+#   "Slime green":"#b3dc6c",
+#   "Desert sand":"#fbe983",
+#   "Macaroni":"#fad165",
+#   "Sea foam":"#92e1c0",
+#   "Pool":"#9fe1e7",
+#   "Denim":"#9fc6e7",
+#   "Rainy sky":"#4986e7",
+#   "Blue velvet":"#9a9cff",
+#   "Purple dino":"#b99aff",
+#   "Mouse":"#8f8f8f",
+#   "Mountain grey":"#cabdbf",
+#   "Earthworm":"#cca6ac",
+#   "Bubble gum":"#f691b2",
+#   "Purple rain":"#cd74e6",
+#   "Toy eggplant":"#a47ae2"
